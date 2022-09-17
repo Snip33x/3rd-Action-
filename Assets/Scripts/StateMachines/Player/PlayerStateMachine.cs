5fa71038b518c4013c1f181278b0ca6f6edbe8bb
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2022-09-17 23:01:43 +0200

    Input Events

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerStateMachine : StateMachine
 {
+    [field: SerializeField] public InputReader InputReader { get; private set; }  //property won't show up in editor, so we use Serialized field but modificated with field:
+
     // Start is called before the first frame update
     private void Start()
     {
