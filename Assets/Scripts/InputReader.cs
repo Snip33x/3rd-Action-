157fa34963250895b6a820f45f1a1dc6c826d5b4
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2022-09-17 22:05:39 +0200

    Handling Inputs

---
+++ Assets/Scripts/InputReader.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputReader : MonoBehaviour, Controls.IPlayerActions
+{
+    public event Action JumpEvent;
+    public event Action DodgeEvent;
+
+    private Controls controls;
+
+    void Start()
+    {
+        controls = new Controls();
+        controls.Player.SetCallbacks(this);
+
+        controls.Player.Enable();
+    }
+
+    private void OnDestroy()
+    {
+        controls.Player.Disable();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (!context.performed) { return; }  //performed means pressed
+
+        JumpEvent?.Invoke(); //if noone is subsctibed to event it will throw error, so we use null check with ?
+    }
+
+    public void OnDodge(InputAction.CallbackContext context)
+    {
+        if (!context.performed) { return; }  //performed means pressed
+
+        DodgeEvent?.Invoke(); //if noone is subsctibed to event it will throw error, so we use null check with ?
+    }
+}
