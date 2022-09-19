a1181a881f91388df50faf90cf01eecb9a8d3f5d
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2022-09-19 21:22:54 +0200

    Player Character Controller

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerStateMachine : StateMachine
 {
     [field: SerializeField] public InputReader InputReader { get; private set; }  //property won't show up in editor, so we use Serialized field but modificated with field:
-
+    [field: SerializeField] public CharacterController Controller { get; private set; }  //property won't show up in editor, so we use Serialized field but modificated with field:
+    [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
     // Start is called before the first frame update
     private void Start()
     {
--- Assets/Scripts/StateMachines/Player/PlayerTestState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -15,12 +15,16 @@ public class PlayerTestState : PlayerBaseState
     public override void Tick(float deltaTime)
     {
         Vector3 movement = new Vector3();
+
         movement.x = stateMachine.InputReader.MovementValue.x;
         movement.y = 0;
         movement.z = stateMachine.InputReader.MovementValue.y;
 
-        stateMachine.transform.Translate(movement * deltaTime); //in fast framerate character will move fast, and in low he will move slow, so we multiply by deltaTime
-        Debug.Log(stateMachine.InputReader.MovementValue);
+        stateMachine.Controller.Move(movement * stateMachine.FreeLookMovementSpeed * deltaTime); //in fast framerate character will move fast, and in low he will move slow, so we multiply by deltaTime
+
+        if(stateMachine.InputReader.MovementValue == Vector2.zero) { return; }
+
+        stateMachine.transform.rotation = Quaternion.LookRotation(movement);
     }
     public override void Exit()
     {
