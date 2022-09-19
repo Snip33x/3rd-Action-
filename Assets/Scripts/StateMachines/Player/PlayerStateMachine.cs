6b5b87f400892985746e852826259a2b14839518
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2022-09-19 22:27:47 +0200

    Movement Blend Tree

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -6,6 +6,7 @@ public class PlayerStateMachine : StateMachine
 {
     [field: SerializeField] public InputReader InputReader { get; private set; }  //property won't show up in editor, so we use Serialized field but modificated with field:
     [field: SerializeField] public CharacterController Controller { get; private set; }  //property won't show up in editor, so we use Serialized field but modificated with field:
+    [field: SerializeField] public Animator Animator { get; private set; }
     [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
     // Start is called before the first frame update
     private void Start()
