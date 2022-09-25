cbbc8637597ea2c632efe443f2a3ce5cc2124414
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2022-09-25 11:55:49 +0200

    ------MELEE COMBAT------- Targeting

---
+++ Assets/Scripts/Combat/Targeting/Targeter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targeter : MonoBehaviour
+{
+    public List<Target> targets = new List<Target>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Target target = other.GetComponent<Target>();
+        
+        if (target == null) { return; }
+       
+        targets.Add(target);
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.TryGetComponent<Target>(out Target target)) { return; }  //same thing as below but written in two lines
+           
+        targets.Remove(target);
+    
+
+        //Target target = other.GetComponent<Target>();
+
+        //if (target == null) { return; }
+
+        //targets.Remove(target);
+
+    }
+}
