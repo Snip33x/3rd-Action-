4af50d5c83bddbd1f4875c2d5b750b246340cffb
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2022-09-25 18:31:44 +0200

    Selecting A target

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -7,6 +7,7 @@ public class PlayerStateMachine : StateMachine
     [field: SerializeField] public InputReader InputReader { get; private set; }  //property won't show up in editor, so we use Serialized field but modificated with field:
     [field: SerializeField] public CharacterController Controller { get; private set; }  //property won't show up in editor, so we use Serialized field but modificated with field:
     [field: SerializeField] public Animator Animator { get; private set; }
+    [field: SerializeField] public Targeter Targeter { get; private set; }
     [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
     [field: SerializeField] public float RotationSmoothValue { get; private set; }
 
